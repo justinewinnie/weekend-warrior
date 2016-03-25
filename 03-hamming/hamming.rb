567ada2ecfa928098307b19d18e84d918105130d
justine <justine.winnie@gmail.com>
2016-03-25 10:28:45 -0700

    .each_char loop works for all tests except last 2 - added unless block to catch strands of diff length. All tests passing.

--- 03-hamming/hamming.rb
+++ 03-hamming/hamming.rb
@@ -1,5 +1,10 @@
 class Hamming
   def self.compute(a, b)
+    # using an unless block! :D
+    unless a.length == b.length
+      raise ArgumentError.new("Cannot compare strands of differing lengths")
+    end
+
     diff = 0
     index = 0
     a.each_char do |letter|
